ae16642670022fdfa95571f003b19e1d1017cd19
Aakash Sigdel <aakash.sigdel@gmail.com>
2019-02-04 23:40:04 +0100

    add additional test to playlist

--- src/Playlist/Playlist.test.tsx
+++ src/Playlist/Playlist.test.tsx
@@ -1,14 +1,29 @@
 import * as React from 'react'
 import { create } from 'react-test-renderer'
+import { shallow, ShallowWrapper } from 'enzyme'
 
-import { Playlist } from './Playlist'
+import { Playlist, IPlaylistProps } from './Playlist'
 import { getVideos } from '../factory'
 
 describe('<Playlist />', () => {
+  let wrapper: ShallowWrapper<IPlaylistProps>
+  const defaultProps: IPlaylistProps = {
+    videos: getVideos(),
+    onClickAdd: jest.fn(),
+    children: (s, k) => <div key={k} />,
+  }
+
+  beforeEach(() => {
+    wrapper = shallow(<Playlist {...defaultProps} />)
+  })
+
   it('should render correctly', () => {
-    const tree = create(
-      <Playlist videos={getVideos()}>{(s, k) => <div key={k} />}</Playlist>
-    ).toJSON()
+    const tree = create(<Playlist {...defaultProps} />).toJSON()
     expect(tree).toMatchSnapshot()
+  })
+
+  it('should call onClickAdd when clickin on add to playlist button', () => {
+    wrapper.find('button').simulate('click')
+    expect(defaultProps.onClickAdd).toHaveBeenCalledTimes(1)
   })
 })
