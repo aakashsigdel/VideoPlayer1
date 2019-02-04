8dc27483506a51534d5c816e524f291c65f37d01
Aakash Sigdel <aakash.sigdel@gmail.com>
2019-02-04 17:39:51 +0100

    add controls to video

--- src/Player/PlayerRoot.tsx
+++ src/Player/PlayerRoot.tsx
@@ -7,7 +7,7 @@ export interface IPlayerRootProps {
   width?: number
   height?: number
   videoUrl: string
-  posterUrl: string
+  posterUrl?: string
 }
 
 export const PlayerRoot: React.FunctionComponent<IPlayerRootProps> = ({
@@ -16,7 +16,13 @@ export const PlayerRoot: React.FunctionComponent<IPlayerRootProps> = ({
   height = DEFAULT_PLAYER_HEIGHT,
   width = DEFAULT_PLAYER_WIDTH,
 }) => (
-  <video height={height} width={width} src={videoUrl} poster={posterUrl}>
+  <video
+    controls
+    height={height}
+    width={width}
+    src={videoUrl}
+    poster={posterUrl}
+  >
     Sorry your browser doesn't support HTML5 videos!
   </video>
 )
