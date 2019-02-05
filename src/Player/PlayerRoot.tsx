6579146775f942d981602ac004bc4f053b7ed972
Aakash Sigdel <aakash.sigdel@gmail.com>
2019-02-05 19:54:18 +0100

    convert fc to class component

--- src/Player/PlayerRoot.tsx
+++ src/Player/PlayerRoot.tsx
@@ -12,23 +12,29 @@ export interface IPlayerRootProps {
   autoPlay?: boolean
 }
 
-export const PlayerRoot: React.FunctionComponent<IPlayerRootProps> = ({
-  videoUrl,
-  posterUrl,
-  height = DEFAULT_PLAYER_HEIGHT,
-  width = DEFAULT_PLAYER_WIDTH,
-  next,
-  autoPlay,
-}) => (
-  <video
-    controls
-    autoPlay={autoPlay}
-    height={height}
-    width={width}
-    src={videoUrl}
-    poster={posterUrl}
-    onEnded={next}
-  >
-    Sorry your browser doesn't support HTML5 videos!
-  </video>
-)
+export class PlayerRoot extends React.Component<IPlayerRootProps> {
+  render() {
+    const {
+      videoUrl,
+      posterUrl,
+      height = DEFAULT_PLAYER_HEIGHT,
+      width = DEFAULT_PLAYER_WIDTH,
+      next,
+      autoPlay,
+    } = this.props
+
+    return (
+      <video
+        controls
+        autoPlay={autoPlay}
+        height={height}
+        width={width}
+        src={videoUrl}
+        poster={posterUrl}
+        onEnded={next}
+      >
+        Sorry your browser doesn't support HTML5 videos!
+      </video>
+    )
+  }
+}
