652073f5f5fd10127dc66cfbacb8a961fe6a3792
Aakash Sigdel <aakash.sigdel@gmail.com>
2019-02-05 20:18:30 +0100

    add play and pause icons

---
+++ src/icons/Pause.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+
+export const Pause: React.FunctionComponent = () => (
+  <svg width="24" height="24" viewBox="0 0 24 24">
+    <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z" />
+    <path d="M0 0h24v24H0z" fill="none" />
+  </svg>
+)
