ca54f17039a7374a4f71a848dcc200bc1449cb04
Aakash Sigdel <aakash.sigdel@gmail.com>
2019-02-06 19:08:12 +0100

    add playlist item

--- src/Playlist/PlaylistItem.tsx
+++ src/Playlist/PlaylistItem.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react'
+import cn from 'classnames'
+
+import { Play, Pause } from '../icons'
 import { IVideo } from '../VideoPlayer'
+import styles from './Playlist.module.css'
 
 export interface IPlaylistItemProps {
   video: IVideo
@@ -11,4 +15,15 @@ export const PlaylistItem: React.FunctionComponent<IPlaylistItemProps> = ({
   video,
   videoNumber,
   onSelectVideo,
-}) => <li onClick={() => onSelectVideo(videoNumber)}>{video.title}</li>
+}) => (
+  <li onClick={() => onSelectVideo(videoNumber)} className={styles.listItem}>
+    <span className={styles.number}>{videoNumber}</span>
+    <span className={cn(styles.title, styles.ellipsis)}>{video.title}</span>
+    <span className={cn(styles.artist, styles.ellipsis)}>{video.artist}</span>
+    {false ? (
+      <Pause style={{ fill: 'black' }} />
+    ) : (
+      <Play style={{ fill: 'black' }} />
+    )}
+  </li>
+)
